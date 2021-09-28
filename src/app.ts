21422533da1e3174444a32dd7879388ad11520d1
Kenji Shimizu <jrywm121@gmail.com>
2021-09-28 09:12:22 +0900

    don't need entrypoint

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import { sort, SortOrder } from "./bitonic-sorter";
-
-var x = [10, 30, 11, 20, 4, 330, 21, 110];
-sort(x, SortOrder.Ascending, (e) => {
-    console.log(e)
-}).then(result => console.log(result))
-
